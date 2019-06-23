0e8c2b0cd2a6f0479e9e5c33801bd3627e79aaa5
Gustavo Paz da Rosa <g.pazdarosa@gmail.com>
2019-06-23 16:34:06 -0300

    arrumando o commit anterior

--- include/infra/handler/IpClientHandler.h
+++ include/infra/handler/IpClientHandler.h
@@ -1,15 +1,21 @@
 
 #include <list>
 #include <stdexcept>
+#include <server/ReplicaManagers.h>
 #include "infra/messaging/MessageHandler.h"
+#include "infra/messaging/Message.h"
+#include "infra/messaging/MessageStreamer.h"
+#include "UploadFileRequest.h"
 
 class IpClientHandler : public MessageHandler {
     //std::list<std::string>& clientList;
+    ReplicaManagers& replicaManagers;
 
 
 
   public:
-    IpClientHandler(){};
+    IpClientHandler(ReplicaManagers& replicaManagers) :
+                    replicaManagers(replicaManagers){};
     void handle(Message message, MessageStreamer& messageStreamer) override;
 
 };
--- src/infra/handler/IpClientHandler.cpp
+++ src/infra/handler/IpClientHandler.cpp
@@ -1,8 +1,10 @@
 #include "infra/handler/IpClientHandler.h"
 #include <sstream>
 
-void IpClientHandler::handle(Message message,
-                                    MessageStreamer& messageStreamer) {
+void IpClientHandler::handle(Message message, MessageStreamer& messageStreamer) {
+    replicaManagers.broadcast(message);
+                                      
     std::string ipclient = message.getBody();
     std::cout << ipclient << std::endl;
+
 }
